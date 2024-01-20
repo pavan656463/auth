efc2836775dfb7039443a418aee7b789b38d0729
pavan6564 <pavan.kumar1@ei.study>
2024-01-20 13:12:01 +0530

    login page

--- authentication/login.php
+++ authentication/login.php
@@ -6,7 +6,7 @@
     <title>Signin</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/auth/authentication/styles/login.css">
 </head>
 <body>
     <div class="row justify-content-center align-items-center" style="height: 100vh;">
