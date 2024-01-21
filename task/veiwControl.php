32f857358880915540aa3db216df5aa78b89957b
pavan6564 <pavan.kumar1@ei.study>
2024-01-21 11:15:54 +0530

    veiwControl

--- task/veiwControl.php
+++ task/veiwControl.php
@@ -1,3 +1,15 @@
 <?php
-echo "Hello world"
-?>
+// form for submit task 
+
+
+
+session_start()  ; 
+
+if(isset($_SESSION['username'])){
+    $username = $_SESSION['username'] ; 
+    echo "Logged as ".$username  ;
+}else{ 
+    echo "Not logged" ; 
+}
+
+?> 
---
+++ task/veiwFunction.php
@@ -0,0 +1,6 @@
+<?php
+
+// function for form 
+
+
+?>
