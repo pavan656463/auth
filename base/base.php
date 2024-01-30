fbe2b4315ee6e79cd65827cbc4bc755b8488f9a8
pavan6564 <pavan.kumar1@ei.study>
2024-01-30 10:47:07 +0530

    views

--- base/base.php
+++ base/base.php
@@ -133,7 +133,7 @@ function getAssignList($conn){
     }
     ?>
     <div class="container" style="margin-left: 0; margin-right:0;">
-        <div class="row">
+        <div class="row" style = "padding-bottom: 20px;">
             <div class="col-lg-6">
                 <div class="card mb-4 box-1">
                     <div class="card-body">
@@ -143,14 +143,35 @@ function getAssignList($conn){
                 </div>
             </div>
             <div class="col-lg-6">
-                <div class="card box-1">
+                <div class="card box-1" >
                     <div class="card-body">
                         <h4 class="card-title">Tasks-Assigned to me </h4>
-                        <?php getAssignList($conn) ?>
+                        <div class="row">
+                            <!-- Pending tasks -->
+                            <div class="col-lg-8">
+                                <div class="card box-1" style="margin-bottom: 20px; padding-bottom: 20px;">
+                                    <div class="card-body">
+                                        <h5>Pending</h5>
+                                    </div>
+                                    <?php getAssignList($conn) ?>
+                                </div>
+                            </div>
+                            <!-- Completed tasks -->
+                            <div class="col-lg-4">
+                                <div class="card box-1">
+                                    <div class="card-body">
+                                        <h5>Completed</h5>
+                                        <!-- Add your content for completed tasks here if needed -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-8">
+        </div>
+    <div class="row">
+        <div class="col-lg-8">
                 <div class="card box-1">
                     <div class="card-body">
                         <h4 class="card-title">Tasks-List</h4>
@@ -176,13 +197,18 @@ function getAssignList($conn){
                     </div>
                 </div>
             </div>
-        </div>
+        </div>  
     </div>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<<<<<<< Updated upstream
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
+=======
+    <script src="/auth/bootstrap-5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+>>>>>>> Stashed changes
 
     </script>
     <script src="/auth/base/scripts/taskListView.js">
@@ -217,9 +243,6 @@ function getAssignList($conn){
             location.reload();
         }, 5000);
     </script>
-
-
-
 
 </body>
 
